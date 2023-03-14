b465c26a6af388bd91ea1e12d0844ef208e73294
Tarek <tarek.awwad.36@gmail.com>
2023-03-14 11:32:09 +0100

    Lint

--- app/app.go
+++ app/app.go
@@ -207,7 +207,7 @@ var (
 // KitoolsApp extends an ABCI application, but with most of its parameters exported.
 // They are exported for convenience in creating helper functions, as object
 // capabilities aren't needed for testing.
-type KitoolsApp struct { // nolint: golint
+type KitoolsApp struct { //nolint: golint
 	*baseapp.BaseApp
 	legacyAmino       *codec.LegacyAmino
 	appCodec          codec.Codec
